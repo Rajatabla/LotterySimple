cff5bcbd55ce3d2e956f679033bb3008663f10d5
Rajatabla <vgubinbox@gmail.com>
2024-11-18 19:27:39 +0100

    Update Main.java
    Refactoring Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,47 +3,24 @@ package main.java.org.example;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-
+/* 
+This is a simple Lottery Game that is combined in one file.
+*/
 public class Main {
-    public static final int COUNT_SET_SIZE = 5;
+    // This is the quantity of Lottery numbers.
+    public static final int COUNT_SET_SIZE = 5; 
 
     public static void main(String[] args) {
-//
-//        oddEvenNumbers(true, 6);
-//        oddEvenNumbers(false, 4);
-//    }
-//
-//    private static void oddNumbers(int num) {
-//        System.out.println(num % 2 != 0);
-//    }
-//
-//    private static void evenNumbers(int num) {
-//        System.out.println(num % 2 == 0);
-//    }
-//
-//    public static void oddEvenNumbers(boolean flag, int num) {
-//        if (flag) {
-//            oddNumbers(num);
-//        } else {
-//            evenNumbers(num);
-//        }
-//    }
-
-
         int[] lotteryNumber = new int[COUNT_SET_SIZE];
         int[] userNumber = new int[COUNT_SET_SIZE];
-
-
         int[] matchNumbers = returnMatchNumbers(lotteryNumber, userNumber);
 
         System.out.println("User entered numbers: " + Arrays.toString(userNumber));
         System.out.println("Winning numbers: " + Arrays.toString(lotteryNumber));
         System.out.println("Matching numbers: " + Arrays.toString(matchNumbers));
-
     }
-
-
-
+    
+    // This method checks if the number belongs to the array
     public static boolean isNumInArray(int num, int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (num == array[i]) {
@@ -52,16 +29,15 @@ public class Main {
         }
         return false;
     }
-
+   
+    // This method returns winning numbers
     public static int[] returnMatchNumbers(int[] arrayOne, int[] arrayTwo) {
         int[] matchNumbers = new int[arrayOne.length];
         int j = 0;
         for (int i = 0; i < arrayOne.length; i++) {
-
             if (isNumInArray(arrayOne[i], arrayTwo)) {
                 matchNumbers[j] = arrayOne[i];
                 j++;
-
             }
         }
         int[] result = new int[j];
@@ -69,18 +45,5 @@ public class Main {
             result[i] = matchNumbers[i];
         }
         return result;
-
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
