fa115b2cf9578311add6e201771b481714d1cd17
Rajatabla <vgubinbox@gmail.com>
2024-11-18 19:37:33 +0100

    Update Utils.java
    Refactoring Utils

--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -2,9 +2,11 @@ package main.java.org.example;
 
 import java.util.Random;
 import java.util.Scanner;
-
+/*
+This Class contains all necessary methods to run Lottery game
+*/
 public class Utils {
-
+    // This method works with user input data
     private static void fillUserArray(int[] array) {
         Scanner scanner = new Scanner(System.in);
         int i = 0;
@@ -19,11 +21,11 @@ public class Utils {
                     array[i] = userEntered;
                     i++;
                 }
-
             }
         }
     }
-
+   
+    // This method checks if the number belongs to array
     public static boolean isNumInArray(int num, int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (num == array[i]) {
@@ -33,8 +35,8 @@ public class Utils {
         return false;
     }
 
+    // This method works with random data
     public static void fillRandomArray(int[] array) {
-
         Random random = new Random();
         int i = 0;
         int randomWin = 0;
@@ -47,39 +49,23 @@ public class Utils {
         }
     }
 
-//    public static void fillArray(int[] array, InputType type) {
-//        switch (type) {
-//
-//            case USER:
-//                fillUserArray(array);
-//                break;
-//
-//            case RANDOM:
-//                fillRandomArray(array);
-//                break;
-//             default
-//        }
-//    }
-
+    // This method fills either one array or another
     public static void fillArraysAuto(int[] array, InputType type) {
         if (type == InputType.USER) {
             fillUserArray(array);
         } else if (type == InputType.RANDOM) {
             fillRandomArray(array);
-
         }
     }
-
-
+    
+    // This method returns winning user numbers
     public static int[] returnMatchNumbers(int[] arrayOne, int[] arrayTwo) {
         int[] matchNumbers = new int[arrayOne.length];
         int j = 0;
         for (int i = 0; i < arrayOne.length; i++) {
-
             if (isNumInArray(arrayOne[i], arrayTwo)) {
                 matchNumbers[j] = arrayOne[i];
                 j++;
-
             }
         }
         int[] result = new int[j];
@@ -87,7 +73,5 @@ public class Utils {
             result[i] = matchNumbers[i];
         }
         return result;
-
     }
 }
-
